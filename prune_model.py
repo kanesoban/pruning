Fixed bug in calculating derivatives

prune_model.py
@@ -3,6 +3,7 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 # Install TensorFlow
 
 import tensorflow as tf
+from tensorflow.keras import datasets
 import numpy as np
 
 # Is this eager mode ?
@@ -14,19 +15,16 @@ from pruning import prune_model
 
 
 BATCH_SIZE = 32
-mnist = tf.keras.datasets.mnist
 
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
+INPUT_SHAPE = (32, 32, 3)
+(train_images, train_labels), (test_images, test_labels) = datasets.cifar10.load_data()
 
-
-data_train = np.expand_dims(x_train / 255.0, axis=-1)
-label_train = np.expand_dims(y_train / 255.0, axis=-1)
-data_test = np.expand_dims(x_train / 255.0, axis=-1)
-label_test = np.expand_dims(y_train / 255.0, axis=-1)
+# Normalize pixel values to be between 0 and 1
+train_images, test_images = train_images / 255.0, test_images / 255.0
 
 model = tf.keras.models.load_model('model.h5')
 
-prune_iterations = 1
-model = prune_model(model, data_train, label_train, data_test, label_test, BATCH_SIZE, prune_iterations)
+prune_iterations = 10
+model = prune_model(model, train_images, train_labels, test_images, test_labels, BATCH_SIZE, prune_iterations)
 
 model.save("pruned_model.h5")

pruning.py
@@ -99,13 +99,19 @@ def unwatch_layer(layer):
     delattr(layer, 'old_call')
 
 
+def unwatch_all_layers(layers):
+    for layer in layers:
+        if hasattr(layer, 'old_call'):
+            unwatch_layer(layer)
+
+
 def calculate_z_derivatives(model: Model, inputs, targets, cnn_layers):
     omega_te = []
-    for i, cnn_layer in enumerate(cnn_layers):
+    for l, cnn_layer in enumerate(cnn_layers):
         total_loss = 0
         with GradientTape() as tape:
-            if i > 0:
-                unwatch_layer(cnn_layers[i-1])
+            if l > 0:
+                unwatch_layer(cnn_layers[l-1])
             watch_layer(cnn_layer, tape)
             outs = model(inputs)
 
@@ -142,7 +148,8 @@ def calculate_z_derivatives(model: Model, inputs, targets, cnn_layers):
         r = np.sum(r, axis=0)
         r /= shape[0] * shape[1] * shape[2]
         omega_te.append(r)
-    return omega_te
+    unwatch_layer(cnn_layers[-1])
+    return np.array(omega_te)
 
 
 def get_cnn_layers(model: tf.keras.Model):
@@ -176,8 +183,10 @@ def prune_model(model: tf.keras.Model, data_train: np.ndarray, label_train: np.n
     cnn_layers = get_cnn_layers(model)
     batches = data_train.shape[0] // batch_size
     for i in tqdm(range(prune_iterations)):
-        omega_te = 0
-        for batch in range(batches):
+        data_batch = data_train[:batch_size]
+        label_batch = label_train[:batch_size]
+        omega_te = calculate_z_derivatives(model, data_batch, label_batch, cnn_layers)
+        for batch in range(1, batches):
             data_batch = data_train[i * batch_size:(i+1) * batch_size]
             label_batch = label_train[i * batch_size:(i+1) * batch_size]
             omega_te += calculate_z_derivatives(model, data_batch, label_batch, cnn_layers)

train_cifar.py
@@ -6,18 +6,20 @@ import tensorflow as tf
 from tensorflow.keras import datasets, layers, models
 import numpy as np
 
+# Is this eager mode ?
+tf.executing_eagerly()
+assert (tf.__version__ == '2.0.0')
+tf.config.experimental_run_functions_eagerly(True)
+
 
 BATCH_SIZE = 32
 mnist = datasets.mnist
 
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
+INPUT_SHAPE = (32, 32, 3)
+(train_images, train_labels), (test_images, test_labels) = datasets.cifar10.load_data()
 
-
-INPUT_SHAPE = (28, 28, 1)
-train_images = np.expand_dims(x_train / 255.0, axis=-1)
-train_labels = np.expand_dims(y_train / 255.0, axis=-1)
-test_images = np.expand_dims(x_test / 255.0, axis=-1)
-test_labels = np.expand_dims(y_test / 255.0, axis=-1)
+# Normalize pixel values to be between 0 and 1
+train_images, test_images = train_images / 255.0, test_images / 255.0
 
 
 model = tf.keras.models.Sequential()
@@ -37,4 +39,5 @@ model.compile(optimizer='adam',
 
 model.fit(train_images, train_labels, validation_data=(test_images, test_labels), batch_size=BATCH_SIZE, epochs=10)
 
-model.save("model.h5")
+model.save("model_dir")
+